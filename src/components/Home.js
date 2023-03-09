8a026b2faddd0ae814b7ce610fe1606283ee49ea
Ana Popovic <franjetina@gmail.com>
2023-03-09 13:03:53 +0100

    spinner component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import Hero from "./Hero";
 import useFetch from "./useFetch";
 import MovieList from "./MovieList";
+import Spinner from "./Spinner";
 
 const Home = () => {
   const { error, isPending, data: searchResults } = useFetch(`https://api.themoviedb.org/3/discover/movie?api_key=ab166ff82684910ae3565621aea04d62`);
@@ -9,7 +10,9 @@ const Home = () => {
     <>
       <Hero text="Welcome to React 201" />
       {error && <div>{error}</div>}
-      {isPending && <div>Loading...</div>}
+      <div className="pos-spinner">
+        {isPending && <Spinner />}
+      </div>
       {/* TODO: make reusable text */}
       {/* <div>Discover</div>  */}
       {searchResults && 
