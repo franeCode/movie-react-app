c2f79c2191ed2f3cafb7ac51a15e9181ed319a85
Ana Popovic <franjetina@gmail.com>
2023-03-10 09:19:55 +0100

    favorite component added

---
+++ src/components/Favorite.js
@@ -0,0 +1,15 @@
+import plusIcon from './plus-icon.svg';
+
+const Favorite = () => {
+
+    
+
+    return ( 
+        <>
+                <img className='icon' src={plusIcon} alt="plus-icon" />
+                Watchlist
+        </>
+     );
+}
+ 
+export default Favorite;
