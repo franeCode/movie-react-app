bc8b0a734fe25d7b5f23fcb1beea497d54abd623
Ana Popovic <franjetina@gmail.com>
2023-03-13 09:30:02 +0100

    home component removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { useState } from 'react';
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+// import Home from "./components/Home";
 import AboutView from "./components/AboutView";
 import SearchView from './components/SearchView';
 import MovieView from './components/MovieView';
 import { Switch, Route } from "react-router-dom";
 import NotFound from './components/NotFound';
-import Favorite from './components/Favorite';
+// import Favorite from './components/Favorite';
 import MovieList from './components/MovieList';
 
 function App() {
